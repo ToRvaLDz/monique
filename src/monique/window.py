ed7ae5a03bc39c4b0799656963f97f1d1ef17627
Marco Migozzi <torvaldz@gmail.com>
2026-02-20 15:27:46 +0100

    Bump version to 0.3.0

--- src/monique/window.py
+++ src/monique/window.py
@@ -486,7 +486,7 @@ class MainWindow(Adw.ApplicationWindow):
         about = Adw.AboutDialog(
             application_name="Monique",
             application_icon="com.github.monique",
-            version="0.2.0",
+            version="0.3.0",
             developer_name="Monique contributors",
             comments="MONitor Integrated QUick Editor for Hyprland and Sway",
             license_type=Gtk.License.GPL_3_0,
